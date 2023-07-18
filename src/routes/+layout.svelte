3c0de19063b46f7714438d208a04ae9b1e92a371
gantrol <gantrol.h@qq.com>
2023-07-18 22:38:51 +0800

    modified: SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,10 @@
 </script>
 
 <svelte:head>
-    <title>英文标点转中文</title>
+    <title>在线英文标点转中文标点工具</title>
+    <meta name="description"
+          content="英文标点转中文标点, 免费, 快速, 准确, 在线, 英文标点转中文"
+    >
 </svelte:head>
 
 <slot />
